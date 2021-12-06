26e73385e8084cff980bd654d1ff77b8bbf93467
AndroidInsight <insightofficialyt@gmail.com>
2021-12-06 21:06:37 +0530

    third

--- lib/home.dart
+++ lib/home.dart
@@ -1,174 +1,196 @@
 import 'package:flutter/material.dart';
+import 'package:walltest/widgets.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 
-class Home extends StatelessWidget {
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1630511389894-061404df9d50?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+  'https://images.unsplash.com/photo-1630563704175-00e6a1245b52?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=330&q=80',
+  'https://images.unsplash.com/photo-1564415637254-92c66292cd64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80',
+  'https://images.unsplash.com/photo-1579054392737-da444c409d46?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+  'https://images.unsplash.com/photo-1602144333475-a2c71f35631a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+  'https://images.unsplash.com/photo-1511677254207-d2db235ccfe1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+];
+
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: ListView(
-            children: [
-              CarouselSlider(
-                items: [
-                  //1st Image of Slider
-
-                  Container(
-                    margin: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://images.unsplash.com/photo-1630511389894-061404df9d50?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-
-                  //2nd Image of Slider
-
-                  Container(
-                    margin: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://images.unsplash.com/photo-1630563704175-00e6a1245b52?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=330&q=80"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-
-                  //3rd Image of Slider
-
-                  Container(
-                    margin: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://images.unsplash.com/photo-1564415637254-92c66292cd64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-
-                  //4th Image of Slider
-
-                  Container(
-                    margin: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://images.unsplash.com/photo-1602144333475-a2c71f35631a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-
-                  //5th Image of Slider
-
-                  Container(
-                    margin: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://images.unsplash.com/photo-1511677254207-d2db235ccfe1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ],
-
-                //Slider Container properties
-
-                options: CarouselOptions(
-                  height: 450.0,
-                  enlargeCenterPage: true,
-                  autoPlay: true,
-                  aspectRatio: 16 / 9,
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  enableInfiniteScroll: true,
-                  autoPlayAnimationDuration: Duration(milliseconds: 800),
-                  viewportFraction: 0.8,
-                ),
-              ),
-            ],
-          ),
-
-
-      ),
-    );
-  }
-}
-
-class NavBar extends StatefulWidget {
-  const NavBar({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<NavBar> {
-  var _selectedTab = _SelectedTab.home;
+class _HomeState extends State<Home> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
 
-  void _handleIndexChanged(int i) {
-    setState(() {
-      _selectedTab = _SelectedTab.values[i];
-    });
-  }
+  final List<Widget> imageSliders = imgList
+      .map((item) => Container(
+            child: Container(
+              margin: EdgeInsets.all(5.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  child: Stack(
+                    children: <Widget>[
+                      Image.network(
+                        item,
+                        fit: BoxFit.cover,
+                        width: 1000.0,
+                        height: 1000,
+                      ),
+                      Positioned(
+                        bottom: 0.0,
+                        left: 0.0,
+                        right: 0.0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromARGB(200, 0, 0, 0),
+                                Color.fromARGB(0, 0, 0, 0)
+                              ],
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                            ),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20.0),
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
+          ))
+      .toList();
+
+  int _currentIndex = 0;
+  final tabs = [
+    Center(
+      child: Container(
+
+      ),
+    ),
+    Center(
+      child: Container(
+        color: Colors.blue,
+      ),
+    ),
+    Center(
+      child: Container(
+        color: Colors.green,
+      ),
+    ),
+    Center(
+      child: Container(
+        color: Colors.orange,
+      ),
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBody: true,
-      body: Container(
-        color: Colors.black,
-        child: Image.asset("lib/img/1.png"),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: brandName(),
+        elevation: 0.0,
       ),
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.only(bottom: 10),
-        child: DotNavigationBar(
-          margin: EdgeInsets.only(left: 10, right: 10),
-          currentIndex: _SelectedTab.values.indexOf(_selectedTab),
-          dotIndicatorColor: Colors.white,
-          unselectedItemColor: Colors.grey[300],
-          // enableFloatingNavBar: false,
-          onTap: _handleIndexChanged,
-          items: [
-            /// Home
-            DotNavigationBarItem(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
               icon: Icon(Icons.home),
-              selectedColor: Color(0xff73544C),
-            ),
-
-            /// Likes
-            DotNavigationBarItem(
-              icon: Icon(Icons.favorite),
-              selectedColor: Color(0xff73544C),
-            ),
-
-            /// Search
-            DotNavigationBarItem(
-              icon: Icon(Icons.search),
-              selectedColor: Color(0xff73544C),
-            ),
-
-            /// Profile
-            DotNavigationBarItem(
+              title: Text('Home'),
+              backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.explore),
+              title: Text('Explore'),
+              backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              title: Text('Category'),
+              backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
               icon: Icon(Icons.person),
-              selectedColor: Color(0xff73544C),
-            ),
-          ],
-        ),
+              title: Text('About'),
+              backgroundColor: Colors.blue),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
+      backgroundColor: Colors.white,
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xfff5f8fd),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    margin: EdgeInsets.symmetric(horizontal: 24),
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                            child: TextField(
+                          decoration: InputDecoration(
+                              hintText: "search wallpapers",
+                              border: InputBorder.none),
+                        )),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ), //SearchBar
+            SizedBox(
+              height: 15,
+            ),
+            Expanded(
+              child: CarouselSlider(
+                items: imageSliders,
+                carouselController: _controller,
+                options: CarouselOptions(
+                    height: 600,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                    aspectRatio: 2.0,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _current = index;
+                      });
+                    }),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList.asMap().entries.map((entry) {
+                return GestureDetector(
+                  onTap: () => _controller.animateToPage(entry.key),
+                  child: Container(
+                    width: 7.0,
+                    height: 7.0,
+                    margin: EdgeInsets.symmetric(horizontal: 4),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: (Theme.of(context).brightness == Brightness.dark
+                                ? Colors.white
+                                : Colors.black)
+                            .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                  ),
+                );
+              }).toList(),
+            ), //IndicatorDots
+            SizedBox(
+              height: 80,
+            )
+          ]),
     );
   }
 }
-
-enum _SelectedTab { home, favorite, search, person }
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
     );
